fd4eb8281beb972eb4c80c92f2a6b1aa21e2ca83
6804062611149 <s6804062611149@email.kmutnb.ac.th>
2025-12-15 10:00:47 +0700

    if x=-1,y=-1,z=-1

---
+++ lab3no7.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int x=-1,y=-1,z=-1;
+    if (x>0) {
+        printf("Black ");
+        if (y>0)
+            printf("Red ");
+        else
+            if (z>0)
+                printf("Blue ");
+            else
+                printf("Green ");
+    }
+    else
+        if (y>0)
+            printf("Pink ");
+        printf("White ");
+}
+
